04faed36f6cbf34b08f8faeed2bbcd7e557abeb4
Shaofeng Tang <shaofeng.tang@intel.com>
2023-08-02 16:34:21 +0800

    Enable upstream release tag intel-onevpl-23.2.4
    - upstream
    https://github.com/oneapi-src/oneVPL-intel-gpu/releases/tag/intel-onevpl-23.2.4
    - Add Android-U support
    - Added base Android.mk files
    - Fixes for build issues seen in Android.
    
    Signed-off-by: Shaofeng Tang <shaofeng.tang@intel.com>

---
+++ _studio/shared/asc/Android.mk
@@ -0,0 +1,97 @@
+LOCAL_PATH:= $(call my-dir)
+
+# =============================================================================
+
+include $(CLEAR_VARS)
+include $(MFX_HOME)/android/mfx_defs.mk
+
+LOCAL_SRC_FILES := $(addprefix src/, asc_avx2_impl.cpp)
+
+LOCAL_C_INCLUDES := \
+    $(MFX_INCLUDES_INTERNAL_HW) \
+    $(MFX_HOME)/_studio/mfx_lib/cmrt_cross_platform/include \
+    $(MFX_HOME)/_studio/mfx_lib/ext/genx/asc/isa
+
+LOCAL_CFLAGS := \
+    $(MFX_CFLAGS_INTERNAL_HW) \
+    -mavx2 \
+    -Wno-error \
+    -Wno-unused-parameter
+
+LOCAL_CFLAGS += -I $(MFX_HOME)/_studio/shared/asc/include/
+
+LOCAL_CFLAGS_32 := $(MFX_CFLAGS_INTERNAL_32)
+LOCAL_CFLAGS_64 := $(MFX_CFLAGS_INTERNAL_64)
+
+LOCAL_HEADER_LIBRARIES := libmfx_gen_headers
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libmfx_asc_avx2
+include $(BUILD_STATIC_LIBRARY)
+
+# =============================================================================
+
+include $(CLEAR_VARS)
+include $(MFX_HOME)/android/mfx_defs.mk
+
+LOCAL_SRC_FILES := $(addprefix src/, asc_sse4_impl.cpp)
+
+LOCAL_C_INCLUDES := \
+    $(MFX_INCLUDES_INTERNAL_HW) \
+    $(MFX_HOME)/_studio/mfx_lib/cmrt_cross_platform/include \
+    $(MFX_HOME)/_studio/mfx_lib/ext/genx/asc/isa
+
+LOCAL_CFLAGS := \
+    $(MFX_CFLAGS_INTERNAL_HW) \
+    -msse4.1 \
+    -Wno-error \
+    -Wno-unused-parameter
+
+LOCAL_CFLAGS_32 := $(MFX_CFLAGS_INTERNAL_32)
+LOCAL_CFLAGS_64 := $(MFX_CFLAGS_INTERNAL_64)
+
+LOCAL_HEADER_LIBRARIES := libmfx_gen_headers
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libmfx_asc_sse4
+include $(BUILD_STATIC_LIBRARY)
+
+# =============================================================================
+
+include $(CLEAR_VARS)
+include $(MFX_HOME)/android/mfx_defs.mk
+
+ASC_SRC_FILES := $(addprefix src/, \
+	asc.cpp \
+	asc_c_impl.cpp \
+	iofunctions.cpp \
+	motion_estimation_engine.cpp \
+	tree.cpp)
+
+LOCAL_SRC_FILES := $(ASC_SRC_FILES)
+
+LOCAL_C_INCLUDES := \
+    $(MFX_INCLUDES_INTERNAL_HW) \
+    $(MFX_HOME)/_studio/mfx_lib/cmrt_cross_platform/include \
+    $(MFX_HOME)/_studio/mfx_lib/ext/genx/asc/isa
+
+LOCAL_STATIC_LIBRARIES := \
+	libmfx_asc_avx2 \
+	libmfx_asc_sse4
+
+LOCAL_CFLAGS := \
+    $(MFX_CFLAGS_INTERNAL_HW) \
+    -msse4.1 \
+    -Wno-error \
+    -Wno-unused-parameter
+
+LOCAL_CFLAGS_32 := $(MFX_CFLAGS_INTERNAL_32)
+LOCAL_CFLAGS_64 := $(MFX_CFLAGS_INTERNAL_64)
+
+LOCAL_HEADER_LIBRARIES := libmfx_gen_headers
+LOCAL_WHOLE_STATIC_LIBRARIES := $(LOCAL_STATIC_LIBRARIES)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libmfx_asc
+
+include $(BUILD_STATIC_LIBRARY)
---
+++ _studio/shared/umc/codec/Android.mk
@@ -0,0 +1,86 @@
+LOCAL_PATH:= $(call my-dir)
+
+# Setting subdirectories to march thru
+MFX_CODEC_LOCAL_DIRS := \
+    vc1_common \
+    jpeg_common \
+    color_space_converter \
+    mpeg2_dec \
+    h265_dec \
+    h264_dec \
+    vc1_dec \
+    jpeg_dec \
+    vp9_dec \
+    av1_dec
+
+MFX_CODEC_LOCAL_SRC_FILES := \
+  $(patsubst $(LOCAL_PATH)/%, %, $(foreach dir, $(MFX_CODEC_LOCAL_DIRS), $(wildcard $(LOCAL_PATH)/$(dir)/src/*.cpp)))
+
+# =============================================================================
+
+include $(CLEAR_VARS)
+include $(MFX_HOME)/android/mfx_defs.mk
+
+LOCAL_SRC_FILES := \
+    brc/src/umc_brc.cpp \
+    brc/src/umc_h264_brc.cpp \
+    brc/src/umc_mpeg2_brc.cpp \
+    brc/src/umc_video_brc.cpp
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/brc/include \
+    $(MFX_INCLUDES_INTERNAL_HW)
+
+LOCAL_CFLAGS := \
+    $(MFX_CFLAGS_INTERNAL_HW) \
+    -Wno-error \
+    -Wno-unused-parameter \
+    -Wno-deprecated-declarations
+
+LOCAL_CFLAGS_32 := $(MFX_CFLAGS_INTERNAL_32)
+LOCAL_CFLAGS_64 := $(MFX_CFLAGS_INTERNAL_64)
+
+LOCAL_HEADER_LIBRARIES := libmfx_gen_headers
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libumc_brc
+
+include $(BUILD_STATIC_LIBRARY)
+
+# =============================================================================
+
+include $(CLEAR_VARS)
+include $(MFX_HOME)/android/mfx_defs.mk
+
+LOCAL_SRC_FILES := $(MFX_CODEC_LOCAL_SRC_FILES)
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/av1_dec/include \
+    $(LOCAL_PATH)/color_space_converter/include \
+    $(LOCAL_PATH)/h264_dec/include \
+    $(LOCAL_PATH)/h265_dec/include \
+    $(LOCAL_PATH)/jpeg_common/include \
+    $(LOCAL_PATH)/jpeg_dec/include \
+    $(LOCAL_PATH)/mpeg2_dec/include \
+    $(LOCAL_PATH)/vc1_common/include \
+    $(LOCAL_PATH)/vc1_dec/include \
+    $(LOCAL_PATH)/vp9_dec/include \
+    $(MFX_INCLUDES_INTERNAL_HW)
+
+LOCAL_CFLAGS := \
+    $(MFX_CFLAGS_INTERNAL_HW) \
+    -Wno-error \
+    -Wno-unused-parameter
+
+LOCAL_CFLAGS_32 := $(MFX_CFLAGS_INTERNAL_32)
+LOCAL_CFLAGS_64 := $(MFX_CFLAGS_INTERNAL_64)
+
+LOCAL_HEADER_LIBRARIES := libmfx_gen_headers
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libumc_codecs_hw
+
+include $(BUILD_STATIC_LIBRARY)
