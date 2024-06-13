6726584ab944640d999afa83195551e75dec1274
Ankit Agarwal <ankit.agarwal@intel.com>
2024-06-13 09:34:12 +0530

    Fixed build error in A15.
    Observed build failure in A15 due to inheritance declaration and
    unsupported api.
    
    Tracked-On: NA
    Signed-off-by: Ankit Agarwal <ankit.agarwal@intel.com>

--- _studio/mfx_lib/vpp/src/mfx_vpp_hw.cpp
+++ _studio/mfx_lib/vpp/src/mfx_vpp_hw.cpp
@@ -2817,7 +2817,7 @@ mfxStatus  VideoVPPHW::Init(
     {
         CmDevice* pCmDevice = QueryCoreInterface<CmDevice>(m_pCore, MFXICORECM_GUID);
 
-        sts = m_SCD.Init(par->vpp.In.CropW, par->vpp.In.CropH, par->vpp.In.Width, par->vpp.In.PicStruct, pCmDevice, CommonCaps::IsCmSupported(m_pCore->GetHWType()));
+        sts = m_SCD.Init(par->vpp.In.CropW, par->vpp.In.CropH, par->vpp.In.Width, par->vpp.In.PicStruct, pCmDevice);
         MFX_CHECK_STS(sts);
 
         m_SCD.SetGoPSize(ns_asc::Immediate_GoP);
